f9f2e179f1873b98baa6601d30c0bb3b94605d10
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2025-01-09 16:39:09 +0700

    Minor changes to github workflow

--- kbh-web/src/app/page.tsx
+++ kbh-web/src/app/page.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
     Card,
-    CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
