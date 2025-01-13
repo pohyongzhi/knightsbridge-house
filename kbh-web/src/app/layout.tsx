ad04cbd75693ab3dab5f19328d98a2d453fd5c0b
f4heemmk.2023 <f4heemmmm@gmail.com>
2025-01-14 02:28:30 +0800

    Added 2 logos only (no words) both in white and purple as pngs. Added footer for the website (final design not finalized), as well as changed the layout.tsx accordingly.

--- kbh-web/src/app/layout.tsx
+++ kbh-web/src/app/layout.tsx
@@ -1,25 +1,30 @@
-import type { Metadata } from "next";
+// Import Statements
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
     title: "Knightsbridge House (Nonthaburi)",
-    description: "Singapore-based Knightsbridge House International School, opening in Nonthaburi, Bangkok (May 2025). Providing exceptional, accessible education focused on academic excellence and holistic development.",
+    description: "Singapore-based Knightsbridge House International School, opening in Nonthaburi, Bangkok in May 2025. Providing exceptional and accessible education focused on academic excellence and holistic development."
 };
 
 export default function RootLayout({
     children,
-}: Readonly<{
+}: Readonly <{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className="min-h-screen bg-background">
-                <div className="max-w-screen-xl mx-auto sm:px-6 lg:px-8">
+        <html lang = "en">
+            <body className = "min-h-screen bg-background">
+                <div className = "max-w-screen-xl mx-auto sm:px-6 lg:px-8">
                     <Header />
                 </div>
-                {children}
+                <main>
+                    {children}
+                </main>
+                <Footer />
             </body>
         </html>
     );
-}
+};
---
+++ kbh-web/src/components/layout/Footer.tsx
@@ -0,0 +1,82 @@
+"use-client";
+
+// Import Statements
+import Link from "next/link";
+import Image from "next/image";
+import { Facebook, Instagram, Mail, Linkedin } from "lucide-react";
+
+export default function Footer() {
+    return (
+        <footer className = "bg-purple-700 text-white py-10">
+            <div className = "container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-8">
+
+                {/* KBH LOGO AND NAME SECTION */}
+                <div className = "space-y-1">
+                    <Image
+                        src = "/images/logo/logo-no-words-white.png"
+                        alt = "Knightsbridge House"
+                        width = {125}
+                        height = {125}
+                        className = "object-contain"
+                    />
+                    <h2 className = "text-3xl font-bold"> Knightsbridge House </h2>
+                    <p className = "text-2xl">
+                        Quality Education
+                        <br />
+                        for Everyone
+                    </p>
+                </div>
+
+                {/* THAILAND BRANCH DETAILS */}
+                <div>
+                    <h3 className = "text-lg font-semibold mb-3"> THAILAND </h3>
+                    <p className = "flex items-center text-sm">
+                        <Mail className = "w-4 h-4 mr-2" />
+                        <a href = "mailto:admissions@kbh.co.th" className = "hover:underline">
+                            admissions@kbh.co.th
+                        </a>
+                    </p>
+                    <p className = "text-sm mt-2">
+                        94-3 Sukha Pracha San 3 Lane, Bang Phut Sub-district, Pak Kret District, Nonthaburi 11120, Thailand
+                    </p>
+                    <h3 className = "text-lg font-semibold mt-9 mb-3"> HOURS </h3>
+                    <p className = "text-sm"> Monday - Friday </p>
+                    <p className = "text-sm mt-2"> 0800 - 1700 </p>
+                </div>
+
+                {/* SINGAPORE BRANCH DETAILS */}
+                <div className = "ml-5">
+                    <h3 className = "text-lg font-semibold mb-3"> SINGAPORE </h3>
+                    <p className = "flex items-center text-sm">
+                        <Mail className = "w-4 h-4 mr-2" />
+                        <a href = "mailto:admissions@kbh.edu.sg" className = "hover:underline">
+                            admissions@kbh.edu.sg
+                        </a>
+                    </p>
+                    <p className = "text-sm mt-2">
+                        67 Jalan Bukit Merah, Connection One, Tower 4 #02-15, S(150167), Singapore
+                    </p>
+                    <h3 className = "text-lg font-semibold mt-9 mb-3"> HOURS </h3>
+                    <p className = "text-sm"> Monday - Friday </p>
+                    <p className = "text-sm mt-2"> 0800 - 1730 </p>
+                </div>
+
+                {/* FOLLOW US SECTION */}
+                <div className = "space-y-4 ml-12">
+                    <h3 className = "text-lg font-semibold mt-36 ml-4"> FOLLOW US </h3>
+                    <div className = "flex space-x-4 mt-4 gap-5">
+                        <Link href = "https://www.google.com" aria-label = "Facebook" className = "hover:text-gray-300 hover:scale-110 transition-transform duration-200">
+                            <Facebook className = "w-6 h-6" />
+                        </Link>
+                        <Link href = "https://www.google.com" aria-label = "Instagram" className = "hover:text-gray-300 hover:scale-110 transition-transform duration-200">
+                            <Instagram className = "w-6 h-6" />
+                        </Link>
+                        <Link href = "https://www.google.com" aria-label = "Linkedin" className = "hover:text-gray-300 hover:scale-110 transition-transform duration-200">
+                            <Linkedin className = "w-6 h-6" />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
